ece56dfebbcda49268d39e90cdb31df70a526239
Emílio <emiliogabriel000@gmail.com>
2023-07-18 22:07:49 -0300

    Correção em componente display

--- calculator/src/Components/display.js
+++ calculator/src/Components/display.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
 const Display = ({ value, hasMemory }) => {
-  const displayValue = hasMemory ? `M ${value}` : value;
+  const displayValue = hasMemory ? `${value}` : value;
+  const displayContainerStyle = {
+    display: 'flex',
+    justifyContent: hasMemory ? 'flex-start' : 'flex-end',
+  };
 
-  return <div className={'display-container'}>{displayValue}</div>;
+  return (
+    <div className="display-container" style={displayContainerStyle}>
+      {hasMemory && <div className="memory-value">M</div>}
+      <div className="number-value">{displayValue}</div>
+    </div>
+  );
 };
 
 export default Display;
